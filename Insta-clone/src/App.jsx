b02a01a4268c5c52bc756e7e8aff5c7d0c02e345
pradeep k <pradeepkarthikeyan19082004@gmail.com>
2025-11-05 13:05:22 +0530

    new project insta clone

---
+++ Insta-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='d-flex vh-100 bg-secondary'>
+    <div className='w-20'>sidebar</div>
+    <div className='w-50 bg-info'>feed</div>
+    <div className='w-30'>suggestions</div>
+    </div>
+  )
+}
+
+export default App
