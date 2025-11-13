74fc05d51b6c11247804eb836976c6d0e4d92631
pradeep k <pradeepkarthikeyan19082004@gmail.com>
2025-11-13 18:14:49 +0530

    commite everything

--- Insta-clone/src/main.jsx
+++ Insta-clone/src/main.jsx
@@ -1,17 +1,14 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-import App from "./App.jsx";
-import ViewStory from "./viewStory.jsx";
-import "./index.css";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/story", element: <ViewStory /> }
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
